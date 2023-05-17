327676156f42e94d4f1ffe818aab7f59c211394a
김유진 <havekdujs5236@naver.com>
2023-05-17 02:29:40 +0000

    5/17 자바웹 수업 완료!!

--- js/login.js
+++ js/login.js
@@ -19,6 +19,7 @@ function login(){
     if(id.value.length === 0 || password.value.length === 0){
         alert("아이디와 비밀번호를 모두 입력해주세요.")
     }else{
+		session_set(); // 세션 생성
         form.submit();
     }
 }
@@ -58,6 +59,8 @@ function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
     id.value = get_id; 
     check.checked = true; 
     }
+    session_check(); // 세션 유무 검사
+
 }
 
 function getCookie(name) {
@@ -74,4 +77,58 @@ function getCookie(name) {
             }
         }
         return ;
+}
+
+function session_set() { //세션 저장
+    let id = document.querySelector("#floatingInput");
+    if (sessionStorage) {
+        sessionStorage.setItem("Session_Storage_test", id.value);
+
+    } else {
+        alert("로컬 스토리지 지원 x");
+    }
+}
+
+
+function session_get() { //세션 읽기
+    if (sessionStorage) {
+       return sessionStorage.getItem("Session_Storage_test");
+    } else {
+        alert("세션 스토리지 지원 x");
+    }
+}
+
+function session_check() { //세션 검사
+    if (sessionStorage.getItem("Session_Storage_test")) {
+        alert("이미 로그인 되었습니다.");
+        location.href='index_login.html'; // 로그인된 페이지로 이동
+    }
+}
+
+function session_del() {//세션 삭제
+    // Check if the sessionStorage object exists
+    if (sessionStorage) {
+        // Retrieve data
+        sessionStorage.removeItem("Session_Storage_test");
+        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+    } else {
+        alert("세션 스토리지 지원 x");
+    }
+}
+
+function encrypt_text(password){
+    const k = "key"; // 클라이언트 키
+    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+    const b = password;
+    const eb = this.encodeByAES256(rk, b);
+    return eb;
+    console.log(eb);
+}
+
+function decrypt_text(){
+    const k = "key"; // 서버의 키
+    const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+    const eb = session_get();
+    const b = this.decodeByAES256(rk, eb);
+    console.log(b); 
 }

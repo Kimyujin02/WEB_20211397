d086d9dafbc0138f6c586fd0ae0e0f10726ac8fd
김유진 <havekdujs5236@naver.com>
2023-06-13 15:29:45 +0000

    응용문제까지 최종 완료~!!!!!!!

--- js/login.js
+++ js/login.js
@@ -1,13 +1,36 @@
-function login(){
+function addJavascript(jsname) { // 자바스크립트 외부 연동
+	var th = document.getElementsByTagName('head')[0];
+	var s = document.createElement('script');
+	s.setAttribute('type','text/javascript');
+	s.setAttribute('src',jsname);
+	th.appendChild(s);
+}
+addJavascript('/js/security.js'); // 암복호화 함수
+addJavascript('/js/session.js'); // 세션 함수
+addJavascript('/js/cookie.js'); // 쿠키 함수
+
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+    let id = document.querySelector("#floatingInput");
+    let check = document.querySelector("#idSaveCheck");
+    let get_id = getCookie("id");
+   
+    if(get_id) { 
+    id.value = get_id; 
+    check.checked = true; 
+    }
+	session_check(); // 세션 유무 검사
+}
+
+function login(){ // 로그인
     let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
     let password = document.querySelector("#floatingPassword");
     let check = document.querySelector("#idSaveCheck");
-
+  
     form.action = "../index_login.html";
-    form.method = "get"
-	
-	if(check.checked == true) { // 아이디 체크 o
+    form.method = "get";
+    
+    if(check.checked == true) { // 아이디 체크 o
             alert("쿠키를 저장합니다.");
             setCookie("id", id.value, 1); // 1일 저장
             alert("쿠키 값 :" + id.value);
@@ -17,18 +40,45 @@ function login(){
     }
     
     if(id.value.length === 0 || password.value.length === 0){
-		validateInput(id,password);
-		alert(id,password);
         alert("아이디와 비밀번호를 모두 입력해주세요.");
     }else{
-		session_set(); // 세션 생성
+        if(login_check()){
+        session_set(); // 세션 생성
         form.submit();
+        login_count(); // 로그인 횟수 체크   
+        }
     }
 }
 function logout(){
+    session_del(); // 세션 삭제
+    logout_count(); 
     location.href='../index.html';
 }
+function login_check(){
+    let id = document.querySelector("#floatingInput");
+    let password = document.querySelector("#floatingPassword");
+    
+    let reg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+    let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    if(regex.test(id.value)){
+    	if(reg.test(password.value)){
+            return true;
+        }
+        else{
+            alert("영문 숫자 특수기호 조합 8자리 이상으로 비밀번호를 입력해주세요.");
+            return false;
+            }
+    }
+    else{
+        alert("@example.com 이메일 주소를 포함해서 입력해주세요");
+        return false;
+        }
+}
 function get_id(){
+    if(true){
+        decrypt_text();
+    }
+    else{
     var getParameters = function(paramName){ // 변수 = 함수(이름)
     var returnValue; // 리턴값을 위한 변수 선언
     var url = location.href; // 현재 접속 중인 주소 정보 저장
@@ -43,48 +93,26 @@ function get_id(){
 		    }
 	    } // 2중 for문 끝
 }; // 함수 끝
-alert(getParameters('id') + '님 반갑습니다!'); // 메시지 창 출력
+alert(getParameters('id') + '님 반갑습니다아!'); // 메시지 창 출력
+	}
 }
 
-function init() { // 로그인 폼에 쿠키에서 가져온 아이디 입력
-  let id = document.querySelector("#floatingInput");
-  let check = document.querySelector("#idSaveCheck");
-  let get_id = getCookie("id");
-
-  if (get_id) {
-    id.value = get_id;
-    check.checked = true;
+function login_count() { // 로그인 횟수 체크
+  let cnt = getCookie("login_cnt");
+  if(cnt == "") {
+    setCookie("login_cnt", 1, 1);
+  }
+  else {
+    setCookie("login_cnt", Number(cnt)+1, 1);
   }
 }
 
-
-//추가 코드
-function validateInput(id, password) {
-  // 아이디와 패스워드에 대한 필터링 패턴 설정
-  var idPattern = /^[a-zA-Z0-9]{4,12}$/; // 4~12자의 영문 대소문자와 숫자
-  var passwordPattern = /^[a-zA-Z0-9!@#$%^&*()]{6,16}$/; // 6~16자의 영문 대소문자, 숫자, 특수문자
-
-  if (!idPattern.test(id)) {
-    alert("아이디는 4~12자의 영문 대소문자와 숫자로 입력해야 합니다.");
-    return false;
+function logout_count() { // 로그아웃 횟수 체크
+  let cnt = getCookie("logout_cnt");
+  if(cnt == "") {
+    setCookie("logout_cnt", 1, 1);
   }
-
-  if (!passwordPattern.test(password)) {
-    alert("비밀번호는 6~16자의 영문 대소문자, 숫자, 특수문자로 입력해야 합니다.");
-    return false;
+  else {
+    setCookie("logout_cnt", Number(cnt)+1, 1);
   }
-
-  return true;
 }
-
-
-function addJavascript(jsname) { // 자바스크립트 외부 연동
-	var th = document.getElementsByTagName('head')[0];
-	var s = document.createElement('script');
-	s.setAttribute('type','text/javascript');
-	s.setAttribute('src',jsname);
-	th.appendChild(s);
-}
-addJavascript('/js/security.js'); // 암복호화 함수
-addJavascript('/js/session.js'); // 세션 함수
-addJavascript('/js/cookie.js'); // 쿠키 함수
--- js/search.js
+++ js/search.js
@@ -1,16 +1,26 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
 var search_array = []; // 빈 배열 – 전역 변수
+var MAX_SEARCH_COUNT = 10;
+var forbidden_words = ["시발", "개새끼", "미친년"]; // 검색 제한 단어
 
-function search_message(){
-   let search_str = document.querySelector("#search_txt");
-    if(search_str.value.length === 0){
-       alert("검색어가 비었습니다. 입력해주세요"); 
+function search_message() {
+  let search_str = document.querySelector("#search_txt");
+
+  if (search_str.value.length === 0) {
+    alert("검색어가 비었습니다. 입력해주세요");
+  } else {
+    if (forbidden_words.includes(search_str.value)) {
+      alert("검색어로 적절하지 않습니다.");
+    } else {
+      alert("검색을 수행합니다!");
+      search_array.push(search_str.value); // 배열에 검색어 추가
+      if (search_array.length > MAX_SEARCH_COUNT) {
+        search_array.shift(); // 맨 앞의 값을 삭제
+      }
+      let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
+      document.querySelector("#form_main").submit();
     }
-    else{
-       alert("검색을 수행합니다!");
-       search_array.push(search_str.value); // 배열에 검색어 추가
-       let text = document.getElementById("search_message").innerHTML = search_array.toString(); // 값 변환
-        document.querySelector("#form_main").submit();
-    }
+  }
 }
+
